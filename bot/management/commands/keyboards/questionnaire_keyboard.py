07575cb0da3d56cdf79ae21ac7e8752dde7885e5
Whom-m0rty <m0rtydisg@gmail.com>
2021-01-19 19:02:31 +0300

    Исправлена офрография

--- bot/management/commands/keyboards/questionnaire_keyboard.py
+++ bot/management/commands/keyboards/questionnaire_keyboard.py
@@ -3,7 +3,7 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
 def add_questionnaire():
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Добавть анкету', color=VkKeyboardColor.POSITIVE)
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button('Добавить анкету', color=VkKeyboardColor.POSITIVE)
     keyboard.add_button('Поиск анкеты', color=VkKeyboardColor.SECONDARY)
     return keyboard.get_keyboard()
